7940f94769df5145170e7680b165ad7615cd0330
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2024-06-20 11:39:36 -0700

    Create Controller

---
+++ src/main/java/com/example/pizza/persistence/repository/PizzaRepository.java
@@ -0,0 +1,7 @@
+package com.example.pizza.persistence.repository;
+
+import com.example.pizza.persistence.entity.PizzaEntity;
+import org.springframework.data.repository.ListCrudRepository;
+
+public interface PizzaRepository extends ListCrudRepository<PizzaEntity, Integer> {
+}
--- src/main/java/com/example/pizza/service/PizzaService.java
+++ src/main/java/com/example/pizza/service/PizzaService.java
@@ -1,23 +1,26 @@
 package com.example.pizza.service;
 
 import com.example.pizza.persistence.entity.PizzaEntity;
+import com.example.pizza.persistence.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class PizzaService {
-    private final JdbcTemplate jdbcTemplate;
+    private final PizzaRepository pizzaRepository;
 
     @Autowired
-    public PizzaService(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+    public PizzaService(PizzaRepository pizzaRepository) {
+        this.pizzaRepository = pizzaRepository;
     }
 
     public List<PizzaEntity> getAll() {
-        return this.jdbcTemplate.query("SELECT * FROM pizza", new BeanPropertyRowMapper<>(PizzaEntity.class));
+        return this.pizzaRepository.findAll();
+    }
+
+    public PizzaEntity get(int idPizza) {
+        return this.pizzaRepository.findById(idPizza).orElse(null);
     }
 }
