adabff0de34879a36f538d38d105fea76dc9504a
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2024-06-21 14:31:50 -0700

    add QueryMethods

--- src/main/java/com/example/pizza/persistence/repository/OrderRepository.java
+++ src/main/java/com/example/pizza/persistence/repository/OrderRepository.java
@@ -3,5 +3,11 @@ package com.example.pizza.persistence.repository;
 import com.example.pizza.persistence.entity.OrderEntity;
 import org.springframework.data.repository.ListCrudRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface OrderRepository extends ListCrudRepository<OrderEntity, Integer> {
+    List<OrderEntity> findAllByDateAfter(LocalDateTime date);
+
+    List<OrderEntity> findAllByMethodIn(List<String>methods);
 }
--- src/main/java/com/example/pizza/service/PizzaService.java
+++ src/main/java/com/example/pizza/service/PizzaService.java
@@ -20,19 +20,36 @@ public class PizzaService {
         return this.pizzaRepository.findAll();
     }
 
+    public List<PizzaEntity> getAvailable() {
+        System.out.println(this.pizzaRepository.countByVeganTrue());
+        return this.pizzaRepository.findAllByAvailableTrueOrderByPrice();
+    }
+
+    public PizzaEntity getByName(String name) {
+        return this.pizzaRepository.findAllByAvailableTrueAndNameIgnoreCase(name);
+    }
+
+    public List<PizzaEntity> getWith(String ingredient) {
+        return this.pizzaRepository.findAllByAvailableTrueAndDescriptionContainingIgnoreCase(ingredient);
+    }
+
+    public List<PizzaEntity> getWithout(String ingredient) {
+        return this.pizzaRepository.findAllByAvailableTrueAndDescriptionNotContainingIgnoreCase(ingredient);
+    }
+
     public PizzaEntity get(int idPizza) {
         return this.pizzaRepository.findById(idPizza).orElse(null);
     }
 
-    public PizzaEntity save(PizzaEntity pizzaEntity) {
-        return this.pizzaRepository.save(pizzaEntity);
-    }
-
-    public boolean exists(int idPizza) {
-        return this.pizzaRepository.existsById(idPizza);
+    public PizzaEntity save(PizzaEntity pizza) {
+        return this.pizzaRepository.save(pizza);
     }
 
     public void delete(int idPizza) {
         this.pizzaRepository.deleteById(idPizza);
+    }
+
+    public boolean exists(int idPizza) {
+        return this.pizzaRepository.existsById(idPizza);
     }
 }
