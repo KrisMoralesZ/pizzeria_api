51dd41629fd551a1eea936881ea5410a6e904870
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2024-06-25 14:29:14 -0700

    Add Cors configuration

--- src/main/java/com/example/pizza/web/config/SecurityConfig.java
+++ src/main/java/com/example/pizza/web/config/SecurityConfig.java
@@ -12,6 +12,8 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
+                .csrf().disable()
+                .cors().and()
                 .authorizeHttpRequests()
                 .anyRequest()
                 .authenticated()
